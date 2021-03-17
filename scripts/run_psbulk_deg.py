Fixed scaling before PCA and refactored the basic pipeline code

scripts/run_psbulk_deg.py
@@ -8,37 +8,43 @@ import os
 input_path = '../qc_data/pseudobulk.h5ad'
 adata = sc.read_h5ad(input_path) 
 
-# Get conditions and remove healthy
-conditions = adata.obs['condition'][adata.obs['condition'] != 'healthy']
+# Get unique conditions and remove healthy
+conditions = np.unique(adata.obs['condition'])
+
+# Get unique cell types
+cell_types = np.unique(adata.obs['cell_type'])
 
 # For each condition and cell type compute DEG and store them in a df
 dfs = []
-for condition in np.unique(conditions):
-    for ctype in np.unique(adata.obs['cell_type']):
-        # Filter by cell type
-        subadata = adata[(adata.obs['cell_type'] == ctype)]
-        # Drop conditions with only one sample
-        subadata = subadata[subadata.obs["condition"].duplicated(keep=False)]
-        # Drop genes that have 0 expression
-        subadata = subadata[:,~(subadata.X == 0).any(axis=0)]
+for cond_a in conditions:
+    for cond_b in conditions:
+        if cond_a != cond_b:
+            for cell_type in cell_types:
+                # Filter by cell type
+                subadata = adata[(adata.obs['cell_type'] == cell_type)]
 
-        # Skip if no samples are available
-        if np.sum(subadata.obs['condition'] == condition) == 0:
-            continue
+                # Drop genes that have 0 expression
+                subadata = subadata[:,~(subadata.X == 0).any(axis=0)]
 
-        # Compute DEG
-        sc.tl.rank_genes_groups(subadata, 'condition', reference='healthy', method='t-test')
+                # Skip if only one sample or less are available
+                if np.sum(subadata.obs['condition'] == cond_a) <= 1 or \
+                   np.sum(subadata.obs['condition'] == cond_b) <= 1:
+                    continue
 
-        # Get df and store it
-        df = sc.get.rank_genes_groups_df(subadata, condition)
-        df['cell_type'] = ctype
-        df['condition'] = condition
-        df.drop('scores', axis=1, inplace=True)
-        dfs.append(df)
+                # Compute DEG
+                sc.tl.rank_genes_groups(subadata, 'condition', groups=[cond_a, cond_b],
+                                        reference=cond_a, method='t-test')
+
+                # Get df and store it
+                df = sc.get.rank_genes_groups_df(subadata, cond_b)
+                df['cell_type'] = cell_type
+                df['contrast'] = '{0}-{1}'.format(cond_b, cond_a)
+                df.drop('scores', axis=1, inplace=True)
+                dfs.append(df)
 
 # Merge all dfs
 df = pd.concat(dfs)
 
 # Save
 os.makedirs('../plot_data/deg/', exist_ok=True)
-df.to_csv('../plot_data/deg/deg.csv', index=False)
+df.to_csv('../plot_data/deg/psbulk_deg.csv', index=False)

scripts/run_psbulk_deg_plots.py
@@ -2,36 +2,40 @@ import numpy as np
 import pandas as pd
 
 import matplotlib.pyplot as plt
+from plotting import volcano
 
 # Read DEG df
-input_path = '../plot_data/deg/deg.csv'
+input_path = '../plot_data/deg/psbulk_deg.csv'
 df = pd.read_csv(input_path)
 
-conditions = np.unique(df['condition'])
+# Get unique contrasts and cell types
+contrasts = np.unique(df['contrast'])
 cell_types = np.unique(df['cell_type'])
 
-for condition in conditions:
+for contrast in contrasts:
     # Define figure
     fig, axes = plt.subplots(3,4, figsize=(4*3, 3*3) , dpi=150, sharey=True, sharex=True)
     axes = axes.flatten()
-    fig.suptitle('{0} vs healthy'.format(condition), fontsize=16)
+    fig.suptitle('{0}'.format(contrast), fontsize=16)
     axes[0].set_visible(False) 
     
     # Get max lfc to set as limit for plot
-    max_lfc = np.max(np.abs(df[df['condition']==condition]['logfoldchanges']))
+    max_lfc = np.max(np.abs(df[df['contrast']==contrast]['logfoldchanges']))
     
     # Volcano for each cell type
     for ctype,ax in zip(cell_types, axes[1:]):
-        deg = df[(df['condition'] == condition) & (df['cell_type'] == ctype)]
+        deg = df[(df['contrast'] == contrast) & (df['cell_type'] == ctype)]
         if deg.shape[0] == 0:
             ax.set_visible(False) 
             continue
-        plot_volcano(ctype, deg, ax, max_lfc)
+        lfcs = np.array(deg['logfoldchanges'])
+        pvals = np.array(deg['pvals'].tolist())
+        volcano(ctype, lfcs, pvals, ax, max_lfc)
     
     # Save figure
     fig.tight_layout()
     fig.set_facecolor('white')
-    fig.savefig('../plots/psbulk_deg_{0}'.format(condition))
+    fig.savefig('../plots/psbulk_deg_{0}'.format(contrast))
 
 axes[0].set_visible(False) 
     

scripts/run_psbulk_norm.py
@@ -3,19 +3,16 @@ import numpy as np
 import pandas as pd
 
 from anndata import AnnData
+from utils import vsn_normalize
 
 '''Generates a new AnnData object of pseudobulk expression profiles per cell type and sample.'''
 
 # Read AnnData object
 input_path = '../qc_data/integrated.h5ad'
 adata = sc.read_h5ad(input_path)
+
 # Filter out unknown cells
 adata = adata[adata.obs['cell_type'] != 'unknown']
-
-
-#adata.obs['condition'] = adata.obs['condition'].astype('str')
-#adata.obs['condition'][adata.obs['condition'] != 'hf_ckd'] = "other"
-#adata.obs['condition'] = adata.obs['condition'].astype('category').cat.reorder_categories(['other', 'hf_ckd'])
 
 X = []
 col_sample_id = []
@@ -35,7 +32,7 @@ for sample_id in sample_ids:
     # Get condition
     condition = ann_adata.obs['condition'].tolist()[0]
     
-    # Open RNA counts and filter by overlaped cells
+    # Open RNA counts and filter by overlaped cells and genes
     input_path = '../data/{0}/outs/filtered_feature_bc_matrix/'.format(sample_id)
     raw_adata = sc.read_10x_mtx(input_path, var_names='gene_symbols', cache=True)
     cell_ids = ['-'.join(c_id.split('-')[:-1]) for c_id in ann_adata.obs.index]
@@ -46,11 +43,12 @@ for sample_id in sample_ids:
     for cell_type in cell_types:
         # Subset annotated data by cell type
         cell_adata = ann_adata[ann_adata.obs['cell_type'] == cell_type]
-        # Subset also raw AnnData
+        
+        # Subset raw AnnData by cell ids from the annotated subset
         cell_ids = ['-'.join(c_id.split('-')[:-1]) for c_id in cell_adata.obs.index]
         craw_adata = raw_adata[cell_ids]
         
-        # Create pseudo bulk profile by summing all counts
+        # Create pseudo bulk profile by summing all counts per gene
         psdo_counts = np.sum(craw_adata.X.toarray(), axis=0)
         
         # Store results
@@ -76,41 +74,29 @@ pb_adata.var.index = ann_adata.raw.var.index
 # Set low counts to 0
 pb_adata.X[pb_adata.X <= 2] = 0
 
-def vsn_normalize(arr):
-    import logging
-    import rpy2.rinterface_lib.callbacks
-    rpy2.rinterface_lib.callbacks.logger.setLevel(logging.ERROR)
-    from rpy2.robjects import pandas2ri
-    pandas2ri.activate()
-    import rpy2.robjects as robjects
-    
-    robjects.globalenv['arr'] = arr
-    arr = robjects.r('''
-            library(vsn)
-            arr <- t(arr)
-            arr[is.nan(arr)] = NA
-            fit <- vsnMatrix(arr)
-            arr <- t(vsn::predict(fit,arr))
-            arr[is.na(arr)] <- 0
-            arr
-            ''')
-    return arr
-
 # VSN normalize
 for ctype in np.unique(pb_adata.obs['cell_type']):
     # Get expression for ctype
     X = pb_adata.X[pb_adata.obs['cell_type'] == ctype].copy()
     Xdim = X.shape
+    
     # Set zeros to nan
     X[X == 0] = np.nan
+    
     # Filter genes not expressed in almost all samples
     msk = np.sum(np.isnan(X), axis=0) < 2
     X = X[:, msk]
+    
     # VSN normalize
     X_norm = vsn_normalize(X)
+    
     # Set filtered genes expr to 0
     X = np.zeros(Xdim)
+    
+    # Add normalized values to matrix
     X[:,msk] = X_norm
+    
+    # Update expr matrix in Anndata object
     pb_adata.X[pb_adata.obs['cell_type'] == ctype] = X
     
 # Write to file

scripts/run_psbulk_norm_plots.py
@@ -5,16 +5,19 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+# Open AnnData object
 input_path = '../qc_data/pseudobulk.h5ad'
 adata = sc.read_h5ad(input_path)
 
-
-
+# Init figure
 fig, axes = plt.subplots(3, 4, figsize=(4*4, 3*4), dpi=150, sharey=True, sharex=True)
 axes = axes.flatten()
 ax = axes[0]
 
+# Set 0 to nan to not bias violins
 adata.X[adata.X == 0] = np.nan
+
+# Plot norm for all cell types
 v_x = np.repeat(adata.obs['sample_id'].tolist(), adata.shape[1])
 v_y = adata.X.flatten()
 v_hue = np.repeat(adata.obs['cell_type'].tolist(), adata.shape[1])
@@ -24,9 +27,9 @@ sns.violinplot(x=v_x, y=v_y, ax=ax, inner="quartile")
 ax.set_xticklabels(ax.get_xticklabels(), rotation=90)
 ax.set_ylabel('Norm expr')
 
+# Plot norm for specific cell types
 for ctype,ax in zip(np.unique(adata.obs['cell_type']), axes[1:]):
     subadata = adata[adata.obs['cell_type'] == ctype].copy()
-    #subadata.X[subadata.X == 0] = np.nan
 
     v_x = np.repeat(subadata.obs['sample_id'].tolist(), subadata.shape[1])
     v_y = subadata.X.flatten()