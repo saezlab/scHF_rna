084c00d755c33976a2a108e9ddd90e4ab76a3374
Pau Badia i Mompel <paubadiamompel@gmail.com>
2021-03-18 11:44:58 +0100

    Refactored QC scripts

--- scripts/run_qc.py
+++ scripts/run_qc.py
@@ -8,19 +8,19 @@ import pickle
 import os
 
 """
-Script to run basic QC filtering. Stores cell meta data for future plotting and writes new AnnData objects.
+Script to run basic QC filtering. Stores data for future plotting and writes new AnnData objects.
 """
 
+# Load meta data
+meta = pd.read_csv('../data/metadata.csv')
+samples = np.unique(meta['sample_id'])
+
+# Load diss genes
 diss_df = pd.read_csv('../data/coregene_df-FALSE-v3.csv').sort_values('PValue').head(200)
 diss_ge = diss_df.gene_symbol.tolist()
 
-meta = {
-    'healthy' : ["CK114","CK115","CK139","CK140"],
-    'acidosis' : ["CK128"],
-    'hf' : ["CK127","CK129","CK135","CK137","CK141"],
-    'hf_ckd' : ["CK116","CK126","CK136","CK138"]
-}
-
+# If needed set up specific thresholds for doublet detection
+# The value should divide the bimodal distirbution in two
 doublet_thresholds = {
     'CK114' : 0.10,
     'CK115' : 0.15,
@@ -38,68 +38,68 @@ doublet_thresholds = {
     'CK141' : 0.20,
 }
 
-for condition, samples in meta.items():
-    for sample in samples:
-        # Read raw data
-        print(sample)
-        input_path = '../data/{0}/outs/filtered_feature_bc_matrix/'.format(sample)
-        adata = sc.read_10x_mtx(input_path,var_names='gene_symbols', cache=True)
-        adata.var_names_make_unique()
-        
-        # Basic filtering
-        sc.pp.filter_cells(adata, min_genes=200)
-        sc.pp.filter_genes(adata, min_cells=3)
-        
-        # Compute QC metrics
-        adata.var['mt'] = adata.var_names.str.startswith('MT-')
-        sc.pp.calculate_qc_metrics(adata, qc_vars=['mt'], percent_top=None, log1p=False, inplace=True)
-        
-        # Compute doublets score
-        sce.pp.scrublet(adata, verbose=False)
-        
-        # Compute dissociation score and normalize it (0-1)
-        sc.tl.score_genes(adata, gene_list=diss_ge, ctrl_size=len(diss_ge), 
-                          score_name='diss_score')
-        adata.obs.diss_score = (adata.obs.diss_score-np.min(adata.obs.diss_score)) / \
-        (np.max(adata.obs.diss_score) - np.min(adata.obs.diss_score))
-        
-        # Set filter values (can be changed)
-        mt_thr = 0.5
-        gene_qnt = 0.99
-        diss_qnt = 0.99
-        
-        if sample in doublet_thresholds:
-            doublet_thr = doublet_thresholds[sample]
-        else:
-            doublet_thr = 0.2
-        
-        # Save cell meta data
-        df = adata.obs[['n_genes_by_counts', 'total_counts', 'pct_counts_mt', 'doublet_score', 'diss_score']]
-        plot_data = {'mt_thr' : mt_thr,
-                     'gene_qnt' : gene_qnt,
-                     'doublet_thr' : doublet_thr,
-                     'diss_qnt' : diss_qnt,
-                     'df' : df
-                    }
-        
-        # Filter
-        gene_thr = np.quantile(adata.obs.n_genes_by_counts, gene_qnt)
-        diss_thr = np.quantile(adata.obs.diss_score, diss_qnt)
-        msk = (adata.obs.n_genes_by_counts < gene_thr) & \
-              (adata.obs.pct_counts_mt < mt_thr) &  \
-              (adata.obs.doublet_score < doublet_thr) & \
-              (adata.obs.diss_score < diss_thr)
-        adata = adata[msk, :]
-        
-        # Normalize
-        sc.pp.normalize_total(adata, target_sum=1e4)
-        
-        # Log transform
-        sc.pp.log1p(adata)
-        
-        # Save results
-        os.makedirs('../plot_data/qc/', exist_ok=True)
-        pickle.dump(plot_data, open('../plot_data/qc/{0}.pkl'.format(sample), "wb"))
-        
-        os.makedirs('../qc_data/', exist_ok=True)
-        adata.write('../qc_data/{0}.h5ad'.format(sample))
+# Perform QC for each sample independently
+for sample in meta['sample_id'].tolist():
+    # Read raw data
+    print(sample)
+    input_path = '../data/{0}/outs/filtered_feature_bc_matrix/'.format(sample)
+    adata = sc.read_10x_mtx(input_path,var_names='gene_symbols', cache=True)
+    adata.var_names_make_unique()
+
+    # Basic filtering
+    sc.pp.filter_cells(adata, min_genes=200)
+    sc.pp.filter_genes(adata, min_cells=3)
+
+    # Compute QC metrics
+    adata.var['mt'] = adata.var_names.str.startswith('MT-')
+    sc.pp.calculate_qc_metrics(adata, qc_vars=['mt'], percent_top=None, log1p=False, inplace=True)
+
+    # Compute doublets score
+    sce.pp.scrublet(adata, verbose=False)
+
+    # Compute dissociation score and normalize it (0-1)
+    sc.tl.score_genes(adata, gene_list=diss_ge, ctrl_size=len(diss_ge), 
+                      score_name='diss_score')
+    adata.obs.diss_score = (adata.obs.diss_score-np.min(adata.obs.diss_score)) / \
+    (np.max(adata.obs.diss_score) - np.min(adata.obs.diss_score))
+
+    # Set filter values (can be changed)
+    mt_thr = 0.5
+    gene_qnt = 0.99
+    diss_qnt = 0.99
+
+    if sample in doublet_thresholds:
+        doublet_thr = doublet_thresholds[sample]
+    else:
+        doublet_thr = 0.2
+
+    # Save cell meta data
+    df = adata.obs[['n_genes_by_counts', 'total_counts', 'pct_counts_mt', 'doublet_score', 'diss_score']]
+    plot_data = {'mt_thr' : mt_thr,
+                 'gene_qnt' : gene_qnt,
+                 'doublet_thr' : doublet_thr,
+                 'diss_qnt' : diss_qnt,
+                 'df' : df
+                }
+
+    # Filter
+    gene_thr = np.quantile(adata.obs.n_genes_by_counts, gene_qnt)
+    diss_thr = np.quantile(adata.obs.diss_score, diss_qnt)
+    msk = (adata.obs.n_genes_by_counts < gene_thr) & \
+          (adata.obs.pct_counts_mt < mt_thr) &  \
+          (adata.obs.doublet_score < doublet_thr) & \
+          (adata.obs.diss_score < diss_thr)
+    adata = adata[msk, :]
+
+    # Normalize
+    sc.pp.normalize_total(adata, target_sum=1e4)
+
+    # Log transform
+    sc.pp.log1p(adata)
+
+    # Save results
+    os.makedirs('../plot_data/qc/', exist_ok=True)
+    pickle.dump(plot_data, open('../plot_data/qc/{0}.pkl'.format(sample), "wb"))
+
+    os.makedirs('../qc_data/', exist_ok=True)
+    adata.write('../qc_data/{0}.h5ad'.format(sample))
--- scripts/run_qc_plots.py
+++ scripts/run_qc_plots.py
@@ -2,18 +2,19 @@ import numpy as np
 import pandas as pd
 import pickle
 
-from utils import *
+import os
+from plotting import *
 
 """
 Script to plot different QC metrics after filtering the data.
 """
 
-meta = {
-    'healthy' : ["CK114","CK115","CK139","CK140"],
-    'acidosis' : ["CK128"],
-    'hf' : ["CK127","CK129","CK135","CK137","CK141"],
-    'hf_ckd' : ["CK116","CK126","CK136","CK138"]
-}
+# Load meta data
+meta = pd.read_csv('../data/metadata.csv')
+samples = np.unique(meta['sample_id'])
+
+# Create plots dir
+os.makedirs('../plots/', exist_ok=True)
 
 # Initialize sumary variables
 total_df = pd.DataFrame(columns=['n_genes_by_counts','total_counts',
@@ -21,71 +22,71 @@ total_df = pd.DataFrame(columns=['n_genes_by_counts','total_counts',
 total_subsets = np.zeros(7).astype(np.int)
 summary_df = []
 total_n_rem = np.zeros(5)
+
 # Run QC plots for each sample and store summary
-for condition, samples in meta.items():
-    for sample in samples:
-        print(sample)
-        input_path = '../plot_data/qc/{0}.pkl'.format(sample)
-        plot_data = pickle.load(open(input_path, "rb"))
-        
-        # Filter params
-        mt_thr = plot_data['mt_thr']
-        gene_qnt = plot_data['gene_qnt']
-        doublet_thr = plot_data['doublet_thr']
-        diss_qnt = plot_data['diss_qnt']
-        df = plot_data['df']
-        gene_thr = np.quantile(df.n_genes_by_counts, gene_qnt)
-        diss_thr = np.quantile(df.diss_score, diss_qnt)
-        
-        # Create QC plots
-        fig, axes = plt.subplots(2,3, figsize=(11,8.5), dpi=150)
-        fig.suptitle('QC metrics {0}'.format(sample), fontsize=11)
-        axes = axes.flatten()
-        
-        # Plot MT
-        plot_mt_vs_counts(df, axes[0], mt_thr=mt_thr)
-        
-        # Plot ngenes
-        plot_ngenes_vs_counts(df, axes[1], gene_thr=gene_thr)
-        
-        # Plot doublet score
-        plot_doublet_scores(df, axes[2], doublet_thr=doublet_thr)
-        
-        # Plot diss score
-        plot_diss_scores(df, axes[3], diss_thr=diss_thr)
-        
-        # Plot diff cells
-        labels = ['MT', 'Gene', 'Doublet', 'Diss', 'Total']
-        msks = np.array([
-            df.pct_counts_mt > mt_thr,
-            df.n_genes_by_counts > gene_thr,
-            df.doublet_score > doublet_thr,
-            df.diss_score > diss_thr
-        ])
-        msks = np.vstack((msks, [np.sum(msks, axis=0) > 0]))
-        n_rem = np.sum(msks, axis=1)
-        total_n_rem += n_rem
-        plot_ncell_diff(df, axes[4], labels=labels, n_rem=n_rem)
-        
-        # Adjust plots
-        axes[5].axis('off')
-        fig.tight_layout()
-        fig.subplots_adjust(top=0.88)
-        fig.set_facecolor('white')
-        
-        # Write to png
-        fig.savefig('../plots/qc_{0}.png'.format(sample))
-        
-        # Filter and append
-        msk = (df.n_genes_by_counts < gene_thr) & \
-          (df.pct_counts_mt < mt_thr) &  \
-          (df.doublet_score < doublet_thr) & \
-          (df.diss_score < diss_thr)
-        
-        df = df[msk]
-        
-        total_df = total_df.append(df, ignore_index=True)
-        summary_df.append([sample, len(msk), sum(msk)])
+for sample in samples:
+    print(sample)
+    input_path = '../plot_data/qc/{0}.pkl'.format(sample)
+    plot_data = pickle.load(open(input_path, "rb"))
+
+    # Filter params
+    mt_thr = plot_data['mt_thr']
+    gene_qnt = plot_data['gene_qnt']
+    doublet_thr = plot_data['doublet_thr']
+    diss_qnt = plot_data['diss_qnt']
+    df = plot_data['df']
+    gene_thr = np.quantile(df.n_genes_by_counts, gene_qnt)
+    diss_thr = np.quantile(df.diss_score, diss_qnt)
+
+    # Create QC plots
+    fig, axes = plt.subplots(2,3, figsize=(11,8.5), dpi=150)
+    fig.suptitle('QC metrics {0}'.format(sample), fontsize=11)
+    axes = axes.flatten()
+
+    # Plot MT
+    plot_mt_vs_counts(df, axes[0], mt_thr=mt_thr)
+
+    # Plot ngenes
+    plot_ngenes_vs_counts(df, axes[1], gene_thr=gene_thr)
+
+    # Plot doublet score
+    plot_doublet_scores(df, axes[2], doublet_thr=doublet_thr)
+
+    # Plot diss score
+    plot_diss_scores(df, axes[3], diss_thr=diss_thr)
+
+    # Plot diff cells
+    labels = ['MT', 'Gene', 'Doublet', 'Diss', 'Total']
+    msks = np.array([
+        df.pct_counts_mt > mt_thr,
+        df.n_genes_by_counts > gene_thr,
+        df.doublet_score > doublet_thr,
+        df.diss_score > diss_thr
+    ])
+    msks = np.vstack((msks, [np.sum(msks, axis=0) > 0]))
+    n_rem = np.sum(msks, axis=1)
+    total_n_rem += n_rem
+    plot_ncell_diff(df, axes[4], labels=labels, n_rem=n_rem)
+
+    # Adjust plots
+    axes[5].axis('off')
+    fig.tight_layout()
+    fig.subplots_adjust(top=0.88)
+    fig.set_facecolor('white')
+
+    # Write to png
+    fig.savefig('../plots/qc_{0}.png'.format(sample))
+
+    # Filter and append
+    msk = (df.n_genes_by_counts < gene_thr) & \
+      (df.pct_counts_mt < mt_thr) &  \
+      (df.doublet_score < doublet_thr) & \
+      (df.diss_score < diss_thr)
+
+    df = df[msk]
+
+    total_df = total_df.append(df, ignore_index=True)
+    summary_df.append([sample, len(msk), sum(msk)])
         
 # Summary plots
 fig, axes = plt.subplots(2,3, figsize=(11,8.5), dpi=150)
