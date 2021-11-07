3939da30184c5813d3fa761052fbeab53fdcc168
PauBadiaM <paubadiamompel@gmail.com>
2021-11-07 23:13:29 +0100

    Added MSigDB analysis

---
+++ scripts/run_psbulk_msigdb.py
@@ -0,0 +1,30 @@
+import numpy as np
+import pandas as pd
+
+import os
+
+from utils import run_ora, get_db
+
+df = pd.read_csv('../plot_data/deg/psbulk_deg.csv')
+df['index'] = df['contrast'] + '$' + df['cell_type']
+contrasts = ['HF-AvsHealthy', 'HF-CKDvsHealthy','HF-CKDvsHF-A']
+
+mat = df.pivot(index='names', columns='index', values='tvals')
+mat[np.isnan(mat)] = 0
+
+network = []
+for row in get_db():
+    gset_id, gset_desc, gset = row
+    for gene in gset:
+        network.append([gset_desc, gene, 1, 1])
+network = pd.DataFrame(network, columns=['source', 'target', 'mor', 'likelihood'])
+network = network.drop_duplicates(['source', 'target'])
+
+# Run ora
+acts = run_ora(mat, network)
+acts[['contrast', 'cell_type']] = acts['condition'].str.split('$', 1, expand=True)
+acts = acts[['source', 'contrast', 'cell_type', 'score', 'p_value']]
+acts = acts.rename(columns={'score' : 'tvals'})
+
+os.makedirs('../plot_data/func/', exist_ok=True)
+acts.to_csv('../plot_data/func/msigdb.csv', index=False)
